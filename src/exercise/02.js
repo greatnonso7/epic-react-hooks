1ddf17f72f6044ff07815625ef09e7e8c120ea99
Ichoku Chinonso <greatchinonso7@gmail.com>
2021-01-25 17:30:25 +0100

    Learnt about custom hooks in react

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,15 +3,34 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+function useLocalStorageState(key, defaultValue = '', {
+  serialize = JSON.stringify, deserialize = JSON.parse
+} = {}) {
+  const [state, setState] = React.useState(
+    () => {
+      const valueInLocalStorage = window.localStorage.getItem(key)
+      if (valueInLocalStorage) {
+        return deserialize(valueInLocalStorage);
+      }
+      return typeof defaultValue === 'function' ? defaultValue() : defaultValue;
+    }
+  )
+  const prevKeyRef = React.useRef(key);
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current;
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey);
+    }
+    prevKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(state));
+  }, [key, serialize, state]);
 
+  return [state, setState];
+}
+
+function Greeting({ initialName = '' }) {
+  const [name, setName] = useLocalStorageState('name', initialName);
   function handleChange(event) {
     setName(event.target.value)
   }
@@ -19,7 +38,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -27,7 +46,11 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  const [count, setCount] = React.useState(0);
+  return <>
+    <button onClick={() => setCount(previousCount => previousCount + 1)}>{count}</button>
+    <Greeting initialName="Greg" />
+  </>
 }
 
 export default App
